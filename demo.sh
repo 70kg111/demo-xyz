8ccfbb3cd7dc1224ccb2efd6547e7fa22e5b51fb
70kg111 <76104718@qq.com>
2018-10-04 15:20:44 +0800

    更新 demo.sh

--- demo.sh
+++ demo.sh
@@ -6,5 +6,8 @@ else
 	cd $1
 	mkdir css js
 	touch index.html css/style.css js/main.js
+	echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > inde.html
+	echo -e "h1{color:red;}" > css/style.css
+	echo -e "var string = \"Hello world\"\nalert(string)" > js/main.js
 	exit 0
 fi
